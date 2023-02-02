d63ddc5d116f15c8da372e4846e6b4eda188171c
Simon <sjwood140263@gmail.com>
2023-02-02 18:29:00 +0000

    Added user input capability.

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,3 @@
-console.log("Git is Awesome!");
-
+const prompt = require("prompt-sync")(); // provides input capability in node
+const name = prompt("What is your name?");
+console.log(`Hello, ${name}`);
